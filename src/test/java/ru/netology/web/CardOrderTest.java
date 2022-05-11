c7a78ca920bc819a8de8c7f16184a1783a8f0af2
Elizar Abramov <nezha2077@mail.ru>
2022-05-11 16:26:58 +0700

    Tests Upgraded

--- src/test/java/ru/netology/web/CardOrderTest.java
+++ src/test/java/ru/netology/web/CardOrderTest.java
@@ -4,11 +4,9 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -28,6 +26,7 @@ public class CardOrderTest {
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999/");
 
     }
 
@@ -40,61 +39,55 @@ public class CardOrderTest {
 
     @Test
     public void shouldSendForm() {
-        driver.get("http://localhost:9999/");
-        List<WebElement> textFields = driver.findElements(By.className("input__control"));
-        textFields.get(0).sendKeys("Роман Козин");
-        textFields.get(1).sendKeys("+79236064545");
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Козин-Терентьев Игорь");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79995556666");
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.className("button__content")).click();
-        String actualText = driver.findElement(By.className("paragraph_theme_alfa-on-white")).getText().trim();
+        String actualText = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText().trim();
         String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
         assertEquals(expected, actualText, "Заявочка не прошла");
     }
 
     @Test
     public void shouldShowErrorNotificationInTheFirstAndLastNameField() {
-        driver.get("http://localhost:9999/");
-        driver.findElement(By.className("input__control")).sendKeys("qqq");
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("qqq");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79995556666");
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.className("button__content")).click();
-        String actualText = driver.findElement(By.className("input__sub")).getText().trim();
+        String actualText = driver.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub")).getText().trim();
         String expected = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
         assertEquals(expected, actualText);
     }
 
     @Test
-    public void shouldShowErrorNotificationInTheFirstAndLastNameFieldEmpty() {
-        driver.get("http://localhost:9999/");
-        driver.findElement(By.className("input__control")).sendKeys("");
+    public void shouldShowErrorNotificationInTheTelephoneNumberField() {
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Козин-Терентьев Игорь");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79995556");
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.className("button__content")).click();
-        String actualText = driver.findElement(By.className("input__sub")).getText().trim();
-        String expected = "Поле обязательно для заполнения";
-        assertEquals(expected, actualText);
-    }
-
-    @Test
-    public void shouldShowErrorNotificationInTheTelephoneNumberField() {
-        driver.get("http://localhost:9999/");
-        List<WebElement> textFieldsControl = driver.findElements(By.className("input__control"));
-        List<WebElement> textFieldsSub = driver.findElements(By.className("input__sub"));
-        textFieldsControl.get(0).sendKeys("Роман Козин");
-        textFieldsControl.get(1).sendKeys("34324324");
-        driver.findElement(By.className("button__content")).click();
-        String actualText = textFieldsSub.get(1).getText().trim();
+        String actualText = driver.findElement(By.cssSelector("[data-test-id='phone'].input_invalid .input__sub")).getText().trim();
         String expected = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
         assertEquals(expected, actualText);
     }
 
     @Test
-    public void shouldShowErrorNotificationInTheTelephoneNumberFieldEmpty() {
-        driver.get("http://localhost:9999/");
-        List<WebElement> textFields = driver.findElements(By.className("input__sub"));
-        driver.findElement(By.className("input__control")).sendKeys("Роман Козин");
+    public void shouldShowErrorNotificationWhenFieldsEmpty() {
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("");
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.className("button__content")).click();
-        String actualText = textFields.get(1).getText();
+        String actualText = driver.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub")).getText().trim();
         String expected = "Поле обязательно для заполнения";
+        assertEquals(expected, actualText);
+    }
+
+    @Test
+    public void shouldShowErrorNotificationWithEmptyCheckbox() {
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Козин-Петров Игорь");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79234560000");
+        driver.findElement(By.className("button__content")).click();
+        String actualText = driver.findElement(By.cssSelector("[data-test-id='agreement'].input_invalid .checkbox__text")).getText().trim();
+        String expected = "Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй";
         assertEquals(expected, actualText);
     }
 }
